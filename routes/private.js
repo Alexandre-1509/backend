676aa099de56347a37294b92be37aa2cbf2977df
AlexandreSoares <soaresallexandre28@gmail.com>
2025-07-31 09:59:05 -0300

    Adicionado rota de cadastrar um novo item no banco de dados, e alterado a rota /estoque para mostrar os items cadastrados no banco, no aqruivos schema.prisma foi adicionado o schema de items

--- routes/private.js
+++ routes/private.js
@@ -5,14 +5,33 @@ const { PrismaClient } = pkg;
 const router = express.Router();
 const prisma = new PrismaClient();
 
-router.get("/estoque", async (req, res) => {
-  try {
-    const users = await prisma.user.findMany()
 
-    res.status(200).json(users)
-  } catch (err) {
-    console.error(err);
+router.post('/novo-item', async (req, res) => {
+  try{
+
+    const itensInfo = req.body
+
+    const novoItem = await prisma.items.create({
+      data:{
+        name: itensInfo.name,
+        quantidade: itensInfo.quantidade
+      }
+    })
+
+    res.status(201).json({ message: 'Item adicionado com sucesso!', novoItem})
+  }catch(err){
+    res.status(400).json('Erro ao adicionar novo produto')
   }
-});
+})
+
+router.get('/estoque', async (req, res) => {
+  try{
+    const itemsListed = await prisma.items.findMany()
+
+    res.status(200).json({message: 'Items listados com sucesso!!!', items: itemsListed})
+  } catch(err){
+    console.log(err)
+  }
+})
 
 export default router
